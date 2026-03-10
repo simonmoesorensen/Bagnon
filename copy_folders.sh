cafd949dda83febe5ed52eb378d86dbcf080ad59
Simon Moe Sørensen <moe.simon@gmail.com>
2026-03-10 08:19:52 +0100

    refactor: :fire: remove dev file

--- copy_folders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DEST="/mnt/c/Program Files/Ascension Launcher/resources/client/Interface/AddOns"
-
-for dir in */; do
-    echo "Copying $dir to $DEST"
-    cp -r "$dir" "$DEST/"
-done
